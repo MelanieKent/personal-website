5b024b0edd5339ade91616103c8f47c70553fc13
melkent <melanie.s.kent@gmail.com>
2023-09-08 21:25:32 -0700

    Changed hero background to linear gradient

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import Typed from 'react-typed';
 
 const Hero = () => {
     return (
-        <div className='text-white bg-[#2c2c2c]'>
+        <div className='text-white bg-gradient-to-b from-[#111118] to-[#393476]'>
             <div id='about' className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
                 <p className='text-[#6683c1] text-xl font-bold p-2'>Hello, I'm</p>
                 <h1 className='text-[#ffffff] md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Melanie Kent</h1>
@@ -13,7 +13,7 @@ const Hero = () => {
                     className='text-[#00d8a4] md:text-4xl sm:text-3xl font-bold md:pl-4 pl-2'
                     strings={['software engineering.', 'digital art.', 'data science.', 'education.']} typeSpeed={50} backSpeed={50} loop/>
                 </div>
-                <p className='md:text-2xl text-xl font-bold text-[#707794]'> 
+                <p className='sm:text-2xl text-md font-bold text-[#707794]'> 
                     A third-year software engineering student @ UBC.
                 </p>
                 <a href="mailto:melanie.s.kent@gmail.com">
